31199af09d14a3ea10feba01b02ef50a05a3b49d
rhrazib <rokibulhasancse@gmail.com>
2022-10-28 23:30:41 +0600

    login ui design

--- app/src/main/java/com/softwaredevschool/myfirstapp/MainActivity.java
+++ app/src/main/java/com/softwaredevschool/myfirstapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.softwaredevschool.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,23 +15,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mEt;
-    private Button mBtn;
-    private TextView mTv;
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEt = findViewById(R.id.nameEt);
-        mBtn = findViewById(R.id.saveBtn);
-        mTv = findViewById(R.id.nameTv);
-
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        nextBtn = findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mEt.getText().toString().trim();
-                mTv.setText(name);
+
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                    if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                     requestPermissions(new String[]{Manifest.permission.CALL_PHONE},1000);
+                    }
+                }
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + 1223));
+                startActivity(intent);
             }
         });
     }
